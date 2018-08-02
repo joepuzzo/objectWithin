d776e7aed4de44cc35f70a04cf3d37702bf32c42
JOSEPH PUZZO <joseph0096.puzzo@libertymutual.com>
2018-08-02 15:28:06 -0400

    updated to give feedback

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function anObject (a) {
 
 // Everything in the small must exist in the large
 
-const matchObjects = ( small, large ) => { 
- 
+const matchObjects = ( small, large ) => {
+
   // Base case1: we are not an object or array
   if( !anArray(small) && !anObject(small) ){
 		const result = small === large;
@@ -38,4 +38,45 @@ const matchObjects = ( small, large ) => {
 
 }
 
+const matchAndCheckObjects = ( small, large, path = 'object' ) => {
+
+  // Base case1: we are not an object or array
+  if( !anArray(small) && !anObject(small) ){
+		const result = small === large;
+    const match = { result, failure: { expected: `${path}: ${small}`, actual: `${path}: ${large}` } };
+		return match;
+	}
+
+	if( anObject(small) ){
+		// For every key in small regur
+  	const keys = Object.keys( small );
+
+    for( key of keys ){
+			// Recur
+			const match = matchAndCheckObjects( small[key], large[key], `${path}.${key}` );
+			// If we did not match return early.. we are done
+			if( !match.result ){
+				return match;
+			}
+    }
+
+		// We are good so return
+		return { result: true, failure: null };
+	}
+
+	// We must be an array so itterate
+	for( let i = 0; i < small.length; i++ ){
+		// Recur
+		const match = matchAndCheckObjects( small[i], large[i], `${path}[${i}]` );
+		// If we did not match return early.. we are done
+		if( !match.result ){
+			return match;
+		}
+	}
+
+	return { result: true, failure: null };
+
+}
+
 module.exports = matchObjects;
+module.exports.objectWithinCheck = matchAndCheckObjects;
--- test/index.js
+++ test/index.js
@@ -1,108 +1,280 @@
 const expect = require('chai').expect;
 const objectWithin = require('../index.js');
+const objectWithinCheck = require('../index.js').objectWithinCheck;
 
 describe('objectWithin', () => {
 
-	it('evaluates two complex objects to true when the smaller object is fully contained within the larger one', () => {
+  describe('objectWithin(small, large)', () => {
+    it('evaluates two complex objects to true when the smaller object is fully contained within the larger one', () => {
 
-		const largeObj = {
-			a: 1,
-			b: 2,
-			c: {
-				d: 3,
-				e: {
-					f: 4, 
-					g: 6
-				},
-				h: 5
-			},
-			e: 7,
-			d: 3, 
-			j: [
-				1,
-				2,
-				3,
-				{
-					a: 7, 
-					b: 8
-				}, 
-				4
-			]
-		}
+  		const largeObj = {
+  			a: 1,
+  			b: 2,
+  			c: {
+  				d: 3,
+  				e: {
+  					f: 4,
+  					g: 6
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			d: 3,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7,
+  					b: 8
+  				},
+  				4
+  			]
+  		}
 
-		const smallObj = {
-			a: 1,
-			c: {
-				e: {
-					f: 4
-				},
-				h: 5
-			},
-			e: 7,
-			j: [
-				1,
-				2,
-				3,
-				{
-					a: 7
-				}
-			]
-		}
+  		const smallObj = {
+  			a: 1,
+  			c: {
+  				e: {
+  					f: 4
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7
+  				}
+  			]
+  		}
 
-		expect( objectWithin(smallObj, largeObj) ).to.be.true;
+  		expect( objectWithin(smallObj, largeObj) ).to.be.true;
 
-	}); 
-	
-	it('evaluates two complex objects to false when the smaller object is NOT fully contained within the larger one', () => {
+  	});
 
-		const largeObj = {
-			a: 1,
-			b: 2,
-			c: {
-				d: 3,
-				e: {
-					f: 4, 
-					g: 6
-				},
-				h: 5
-			},
-			e: 7,
-			d: 3, 
-			j: [
-				1,
-				2,
-				3,
-				{
-					a: 7, 
-					b: 8
-				}, 
-				4
-			]
-		}
+  	it('evaluates two complex objects to false when the smaller object is NOT fully contained within the larger one', () => {
 
-		const smallObj = {
-			a: 1,
-			i: 1, // <<<<<<<
-			c: {
-				e: {
-					f: 4
-				},
-				h: 5
-			},
-			e: 7,
-			j: [
-				1,
-				2,
-				3,
-				{
-					a: 7
-				}
-			]
-		}
+  		const largeObj = {
+  			a: 1,
+  			b: 2,
+  			c: {
+  				d: 3,
+  				e: {
+  					f: 4,
+  					g: 6
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			d: 3,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7,
+  					b: 8
+  				},
+  				4
+  			]
+  		}
 
-		expect( objectWithin(smallObj, largeObj) ).to.be.false;
+  		const smallObj = {
+  			a: 1,
+  			i: 1, // <<<<<<<
+  			c: {
+  				e: {
+  					f: 4
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7
+  				}
+  			]
+  		}
 
-	}); 
+  		expect( objectWithin(smallObj, largeObj) ).to.be.false;
+
+  	});
+
+  });
+
+
+  describe('objectWithinCheck(small, large)', () => {
+    it('evaluates two complex objects to true when the smaller object is fully contained within the larger one', () => {
+
+  		const largeObj = {
+  			a: 1,
+  			b: 2,
+  			c: {
+  				d: 3,
+  				e: {
+  					f: 4,
+  					g: 6
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			d: 3,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7,
+  					b: 8
+  				},
+  				4
+  			]
+  		}
+
+  		const smallObj = {
+  			a: 1,
+  			c: {
+  				e: {
+  					f: 4
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7
+  				}
+  			]
+  		}
+
+  		expect( objectWithinCheck(smallObj, largeObj) ).to.deep.equal({result: true, failure: null});
+
+  	});
+
+  	it('evaluates two complex objects to false when the smaller object is NOT fully contained within the larger one', () => {
+
+  		const largeObj = {
+  			a: 1,
+  			b: 2,
+  			c: {
+  				d: 3,
+  				e: {
+  					f: 4,
+  					g: 6
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			d: 3,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7,
+  					b: 8
+  				},
+  				4
+  			]
+  		}
+
+  		const smallObj = {
+  			a: 1,
+  			i: 1, // <<<<<<<
+  			c: {
+  				e: {
+  					f: 4
+  				},
+  				h: 5
+  			},
+  			e: 7,
+  			j: [
+  				1,
+  				2,
+  				3,
+  				{
+  					a: 7
+  				}
+  			]
+  		}
+
+  		expect( objectWithinCheck(smallObj, largeObj) ).to.deep.equal({
+        result: false,
+        failure: {
+          actual: 'object.i: undefined',
+          expected: 'object.i: 1'
+        }
+      });
+
+  	});
+
+
+    it('evaluates two complex objects to false when the smaller object is NOT fully contained within the larger one', () => {
+
+      const largeObj = {
+        a: 1,
+        b: 2,
+        c: {
+          d: 3,
+          e: {
+            f: 4,
+            g: 6
+          },
+          h: 5
+        },
+        e: 7,
+        d: 3,
+        j: [
+          1,
+          2,
+          3,
+          {
+            a: 7,
+            b: 8
+          },
+          4
+        ]
+      }
+
+      const smallObj = {
+        a: 1,
+        c: {
+          e: {
+            f: 4
+          },
+          h: 5
+        },
+        e: 7,
+        j: [
+          1,
+          2,
+          3,
+          {
+            a: 8
+          }
+        ]
+      }
+
+      expect( objectWithinCheck(smallObj, largeObj) ).to.deep.equal({
+        result: false,
+        failure: {
+          actual: 'object.j[3].a: 7',
+          expected: 'object.j[3].a: 8'
+        }
+      });
+
+    });
+
+  });
+
 
 });
-
